ba310d90b012ee8f8e8aa28b033cfc15fc1b4aab
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-04 10:07:53 -0500

    Inserted record names

---
+++ albums_seeder.sql
@@ -0,0 +1,65 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ("Michael Jackson", "Thriller", "1982", 46000000,"Pop, Rock, R&B"),
+
+("AC/DC", "Back in Black", "1980", 26100000,"Hard Rock"),
+
+("Pink Floyd", "The Dark Side of the Moon", "1973", 24200000, "Progressive Rock"),
+
+("Whitney Houston", "The Bodyguard", "1992", 27400000, "Soundtrack/R&B, Soul, Pop"),
+
+("Meat Loaf","Bat Out of Hell","1977",20600000,"Hard Rock, Progressive Rock"),
+
+("Eagles","Their Greatest Hits 1971–1975)","1976",32200000,"Rock, Soft Rock, Folk Rock"),
+
+("Bee Gees","Saturday Night Fever","1977",20600000,"Disco"),
+
+("Fleetwood Mac","Rumours","1977",27900000,"Soft Rock"),
+
+("Shania Twain","Come On Over","1997",29600000,"Country, Pop"),
+
+("Led Zeppelin","Led Zeppelin IV","1971",29000000,"Hard Rock, Heavy Metal"),
+
+("Michael Jackson","Bad","1987",20300000,"Pop, Funk, Rock"),
+
+("Alanis Morissette","Jagged Little Pill","1995",24800000,"Alternative Rock"),
+
+("Celine Dion","Falling into You","1996",20200000,"Pop, Soft Rock"),
+
+("The Beatles","Sgt. Peppers Lonely Hearts Club Band","1967",13100000,"Rock"),
+
+("Eagles","Hotel California","1976",21500000,"Rock, Soft Rock, Folk Rock"),
+
+("Mariah Carey","Music Box","1993",19000000,"Pop, R&B, Rock"),
+
+("Michael Jackson","Dangerous","1991",17600000,"Rock, Funk, Pop"),
+
+("Various artists","Dirty Dancing","1987",17900000,"Pop, Rock, R&B"),
+
+("Celine Dion","Lets Talk About Love","1997",19300000,"Pop, Soft Rock"),
+
+("The Beatles","1","2000",21600000,"Rock"),
+
+("Adele","21","2011",22300000,"Pop, Soul"),
+
+("The Beatles","Abbey Road","1969",14400000,"Rock"),
+
+("Bruce Springsteen","Born in the U.S.A.","1984",19600000,"Rock"),
+
+("Dire Straits","Brothers in Arms","1985",17700000,"Rock"),
+
+("James Horner","Titanic: Music from the Motion Picture","1997",18100000,"Soundtrack"),
+
+("Madonna","The Immaculate Collection","1990",19400000,"Pop, Dance"),
+
+("Metallica","Metallica","1991",19900000,"Thrash Metal, Heavy Metal"),
+
+("Nirvana","Nevermind","1991",16700000,"Grunge, Alternative Rock"),
+
+("Pink Floyd","The Wall","1979",17600000,"Progressive Rock"),
+
+("Santana","Supernatural","1999",20500000,"Rock"),
+
+("Guns N Roses","Appetite for Destruction","1987",21300000,"Heavy Metal, Hard Rock");
+
