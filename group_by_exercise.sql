994fae9332ce94471cb88926210c28a14c150bf3
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-05 14:10:22 -0500

    grouped by first name then last name

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -4,7 +4,7 @@ SELECT DISTINCT title
 FROM titles
 GROUP BY title;
 
-SELECT DISTINCT last_name
+SELECT *
 FROM employees
 Where last_name like 'E%' AND last_name like '%E'
-GROUP BY last_name;
+GROUP BY first_name, last_name;
