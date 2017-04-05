47d5a8d29125eef3087ac7409be8cb4f267c61a7
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-05 16:10:55 -0500

    used count, concat and order by to find unique numbers of names

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -15,3 +15,10 @@ FROM employees
 Where birth_date LIKE '%12-25'
 	AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
 ORDER BY birth_date, hire_date DESC;
+
+SELECT CONCAT(count(*),' ', first_name, ' ', last_name) 
+FROM employees
+Where last_name LIKE '%q%'
+	AND last_name NOT LIKE '%qu%'
+GROUP BY last_name, first_name
+ORDER BY CONCAT(count(*),' ', first_name, ' ', last_name) DESC;
