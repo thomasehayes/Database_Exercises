ec6714cb2d55d5d8583f8cc7de3047249de3e1fa
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-05 10:10:51 -0500

    Finished Where clause exercise

---
+++ where_exercises.sql
@@ -0,0 +1,17 @@
+USE employee.sql
+
+SELECT first_name
+FROM employees
+Where first_name IN ('Irena', 'Vidya','Maya');
+
+SELECT *
+FROM employees
+Where hire_date between '1990-01-01' AND '1999-12-31';
+
+SELECT *
+FROM employees
+Where birth_date LIKE '%12-25';
+
+SELECT * 
+FROM employees
+Where last_name like '%q%';
