2e8b862d6c1e11b28bd0893d181bd95db22f6df0
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-05 12:21:38 -0500

    reversed sort order for both queries

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -6,17 +6,9 @@ Where (first_name ='Irena'
 OR first_name = 'Vidya' 
 OR first_name ='Maya')
 AND gender = 'M'
-ORDER BY first_name;
-
-SELECT *
-FROM employees
-Where (first_name ='Irena'
-OR first_name = 'Vidya' 
-OR first_name ='Maya')
-AND gender = 'M'
-ORDER BY last_name, first_name;
+ORDER BY last_name DESC, first_name;
 
 SELECT *
 FROM employees
 Where last_name like 'E%' AND last_name like '%E'
-ORDER BY emp_no;
+ORDER BY emp_no DESC;
