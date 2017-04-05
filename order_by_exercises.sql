67db81942a9b2e3c505a211129863d8aa1aca5f5
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-05 12:23:29 -0500

    Sorted employee birthday on 12-25 from oldest and hire date by last person to be hired

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -12,3 +12,9 @@ SELECT *
 FROM employees
 Where last_name like 'E%' AND last_name like '%E'
 ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+Where birth_date LIKE '%12-25'
+AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+ORDER BY birth_date, hire_date DESC;
