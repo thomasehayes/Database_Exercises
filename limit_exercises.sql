a23e62ce8b486de0fb431ae0c10a4cca769d9483
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-05 15:38:05 -0500

    Adding Ryan's walkthrough

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,11 +5,12 @@ FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
 
-SELECT *
+SELECT first_name, last_name
 FROM employees
 Where birth_date LIKE '%12-25'
 	AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
-ORDER BY birth_date, hire_date DESC;
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
 
 SELECT first_name, last_name
 FROM employees
