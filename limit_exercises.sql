5a44342b700cdf894fc343810b2ddce63d50fa74
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-05 15:22:18 -0500

    finished limit exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,9 +5,15 @@ FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
 
+SELECT *
+FROM employees
+Where birth_date LIKE '%12-25'
+	AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+ORDER BY birth_date, hire_date DESC;
+
 SELECT first_name, last_name
 FROM employees
 Where birth_date LIKE '%12-25'
 	AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
 ORDER BY birth_date, hire_date DESC
-LIMIT 5;
+LIMIT 5 OFFSET 45;
