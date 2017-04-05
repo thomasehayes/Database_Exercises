050cf354b0cb3ef3f273f5c1c5fba7848599304c
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-05 14:48:36 -0500

    Showing the first 5 from previous exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,4 +2,12 @@ use employees;
 
 SELECT DISTINCT last_name
 FROM employees
-ORDER BY last_name DESC;
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT first_name, last_name
+FROM employees
+Where birth_date LIKE '%12-25'
+	AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
